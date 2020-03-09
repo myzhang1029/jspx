1d47568fa8095e9fd6e2e45ec4f4316f7e85de59
Zhang Maiyun <myzhang1029@hotmail.com>
2020-03-09 16:16:01 +0800

    Create release

--- www/sw.js
+++ www/sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.06e336a9.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.db746d13.js')};importScripts('conf.js')
